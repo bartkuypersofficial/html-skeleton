53925fd40fd2d2e393da269cf19d2b6f74079aba
Bart Kuijpers <info@bartkuypers.nl>
2025-01-27 15:15:11 +0100

    Create gulpfile.js

---
+++ src/gulpfile.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const { watch, parallel, series, src, dest } = require('gulp');
+const del = require('delete');
+const sass = require('gulp-sass')(require('sass'));
+const gcmq = require('gulp-group-css-media-queries');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+const sassLint = require('gulp-sass-lint');
+const insert = require('gulp-insert');
+
+function clean(cb) {
+  del(['../css', '../js'], { force: true }, cb);
+}
+
+function styles(cb) {
+  return src('assets/scss/**/*.scss')
+    .pipe(sass().on("error", sass.logError))
+	  .pipe(gcmq())
+    .pipe(dest('../css'))
+    .pipe(postcss([ 
+	    autoprefixer(), 
+	    cssnano() 
+	  ]))
+    .pipe(rename({
+      extname: '.min.css'
+    }))
+    .pipe(dest('../css'));
+  cb();
+}
+
+function scsslint(cb) {
+  return src('assets/scss/**/*.scss')
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
+  cb();
+}
+
+function scripts(cb) {
+  return src(['assets/scripts/main.js', 'assets/scripts/**/*.js'])
+	  .pipe(insert.append('\n\n'))  
+    .pipe(concat('main.js'))
+	  .pipe(dest('../js'))
+    .pipe(uglify())
+    .pipe(rename({
+      extname: '.min.js'
+    }))
+    .pipe(dest('../js'));
+  cb();
+}
+
+function watching() {
+	watch('assets/scss/**/*.scss', parallel(styles, scsslint));
+	watch('assets/scripts/**/*.js', parallel(scripts));
+}
+
+exports.clean = clean;
+exports.styles = styles;
+exports.scsslint = scsslint;
+exports.scripts = scripts;
+exports.watching = watching;
+exports.default = series([clean], styles, scsslint, scripts, watching)
